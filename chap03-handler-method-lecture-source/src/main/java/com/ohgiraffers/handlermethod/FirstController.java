464989a2af9ee9882c178737dc11c9388f7301a2
yerincho06 <yerincho06@gmail.com>
2024-02-16 09:49:46 +0900

    @RequestParam

--- chap03-handler-method-lecture-source/src/main/java/com/ohgiraffers/handlermethod/FirstController.java
+++ chap03-handler-method-lecture-source/src/main/java/com/ohgiraffers/handlermethod/FirstController.java
@@ -6,7 +6,10 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.context.request.WebRequest;
+
+import java.util.Map;
 
 @Controller
 
@@ -21,6 +24,7 @@ public class FirstController {
 //    }
     public void regist(){}
 
+    /* 설명. request를 쓰게 된다면 request 개념은 사용자의 입력값이 존재, model은 동적 페이지의 재료를 담는 용도로 쓰자. */
     @PostMapping("regist")
 //    public String registMenu(HttpServletRequest request){
     public String registMenu(WebRequest request, Model model){
@@ -37,4 +41,44 @@ public class FirstController {
 
         return "first/messagePrinter";
     }
+
+    @GetMapping("modify")
+    public void modify(){}
+
+    /* 설명.
+     *  request의 parameter로 넘어오는 값들의 key값과 일치하는 변수명을 작성하고 @RequestParam
+     *  어노테이션을 적용하면 알아서 값을 꺼내고 해당 매개변수의 자료형에 맞게 변환까지 해 준다.
+     *  @RequestParam 어노테이션은 생략도 가능하다.
+     *  (스프링 부터는 컨트롤러의 파싱(Parsing) 작업이 간소화 된다는 것을 알 수 있다.)
+    **/
+
+    /* 설명.
+     *  @RequestParam의 속성들
+     *  1. defaultValue: 사용자의 입력 값이 없거나("") request의 parameter 키 값과 일치하지 않는
+     *                   매개변수 명 사용 시 매개변수가 가질 기본 default값을 작성한다.
+     *  2. name: request parameter의 키 값과 다른 매개변수 명을 사용하고 싶을 때 request parameter의
+     *           키 값을 작성한다.
+    **/
+    @PostMapping("modify")
+    public String modifyMenu(Model model, @RequestParam(defaultValue="디폴트", name = "name") String modifyName,
+                                            @RequestParam(defaultValue = "0") int modifyPrice){
+
+        String message = modifyName + "메뉴의 가격을 " + modifyPrice + "(으)로 변경하였습니다.";
+        model.addAttribute("message", message);
+
+        return "first/messagePrinter";
+    }
+
+    @PostMapping("modify2")
+    public String modifyMenu(Model model, @RequestParam Map<String, String> parameter){
+
+        String modifyName = parameter.get("name2");
+        int modifyPrice = Integer.valueOf(parameter.get("modifyPrice2"));
+
+        String message = modifyName + "메뉴의 가격을 " + modifyPrice + "(으)로 변경하였습니다.";
+        model.addAttribute("message", message);
+
+        return "first/messagePrinter";
+    }
+
 }
