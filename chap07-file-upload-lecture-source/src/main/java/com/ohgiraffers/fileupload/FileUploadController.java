d76f3453ac72562ddb294b21aa0a5360c60bf67d
yerincho06 <yerincho06@gmail.com>
2024-02-19 14:04:38 +0900

    path

--- chap07-file-upload-lecture-source/src/main/java/com/ohgiraffers/fileupload/FileUploadController.java
+++ chap07-file-upload-lecture-source/src/main/java/com/ohgiraffers/fileupload/FileUploadController.java
@@ -12,8 +12,7 @@ import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.io.File;
 import java.io.IOException;
-import java.util.List;
-import java.util.UUID;
+import java.util.*;
 
 @Controller
 public class FileUploadController {
@@ -67,12 +66,56 @@ public class FileUploadController {
     }
 
     @PostMapping("multi-file")
-    public String multiFileUpload(@RequestParam List<MultipartFile> mulFile,
+    public String multiFileUpload(@RequestParam List<MultipartFile> multiFiles,
                                   @RequestParam String multiFileDescription,
-                                  RedirectAttributes rttr) {
+                                  RedirectAttributes rttr) throws IOException {
+//        System.out.println("multiFile = " + multiFile);
+//        System.out.println("multiFileDescription = " + multiFileDescription);
+        String filePath = resourceLoader.getResource("classpath:static/uploadFiles/img/multi/")
+                        .getFile()
+                        .getAbsolutePath();
+
+        /* 설명. 사용자가 올린 파일들을 다 rename하고 저장하는 작업 및 DB로 전달하기 위한 List에 쌓기 */
+        List<Map<String, String>> files = new ArrayList<>();    // DB에 저장할 값들을 지닌 List
+        List<String> saveFiles = new ArrayList<>();             // 화면단에서 img 태그가 참조할 정적 리소스 경로(src 경로)
+
+        try {
+            for (int i = 0; i < multiFiles.size(); i++) {
+                String originFileName = multiFiles.get(i).getOriginalFilename();
+                String ext = originFileName.substring(originFileName.lastIndexOf("."));
+                String savedName = UUID.randomUUID().toString().replace("-", "") + ext;
+
+                /* 설명. DB에 저장할 사용자가 올린 하나의 파일 정보(DB 테이블 컬럼을 참고할 것) */
+                Map<String, String> file = new HashMap<>();
+                file.put("originFileName", originFileName);
+                file.put("savedName", savedName);
+                file.put("filePath", filePath);
+                file.put("multiFileDescription", multiFileDescription);
+
+                files.add(file);
+
+                multiFiles.get(i).transferTo(new File(filePath + "/" + savedName));
+                saveFiles.add("uploadFiles/img/multi/" + savedName);
+            }
+
+            /* 설명. 여기까지 문제가 없었다면 화면의 재료를 던져준다. */
+            rttr.addFlashAttribute("message", "다중 파일 업로드 성공!");
+            rttr.addFlashAttribute("imgs", saveFiles);
+            rttr.addFlashAttribute("multiFileDescription", multiFileDescription);
+        } catch (Exception e) {
+
+            /* 설명. List<Map<String, String>> files로 쌓인 업로드 된 파일들을 찾아 일일이 다시 지운다. */
+            for(int i= 0; i < files.size(); i++ ) {
+                Map<String, String> file = files.get(i);
+                new File(filePath + "/" + file.get("savedName")).delete();
+            }
+
+            /* 설명. 실패 했다면 실패 메시지를 전달한다. */
+            rttr.addFlashAttribute("message", "파일 업로드 실패!");
+        }
+
         return "redirect:/result";
     }
-
 
     @GetMapping("result")
     public void result() {}
